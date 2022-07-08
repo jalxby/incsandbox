c248b2e9fe9e467e262de06c5ff1094cd1fa3c92
Aleksander Jaroshevich <jalxby@gmail.com>
2022-07-08 18:43:22 +0300

    Commit freecodecamp - 4

--- freecodecamp/BasicJavaScript/Store Multiple Values in one Variable using JavaScript Arrays.js
+++ freecodecamp/BasicJavaScript/Store Multiple Values in one Variable using JavaScript Arrays.js
@@ -92,3 +92,92 @@ const myArray = [["John", 23], ["dog", 3]];
 // Only change code below this line
 const removedFromMyArray = myArray.shift();
 
+//Manipulate Arrays With unshift()
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+// Only change code below this line
+myArray.unshift(["Paul", 35])
+
+//Shopping List
+const myList = [["Milk", 10], ["Bred", 6], ["Meat", 75], ["Pizza", 56], ["Peperoni", 36]];
+
+//Write Reusable JavaScript with Functions
+function reusableFunction(){
+    console.log("Hi World");
+}
+reusableFunction();
+
+//Passing Values to Functions with Arguments
+function functionWithArgs(a, b){
+    console.log(a + b);
+}
+
+//Return a Value from a Function with Return
+function timesFive(a){
+    return a * 5;
+}
+
+//Global Scope and Functions
+// Declare the myGlobal variable below this line
+let myGlobal = 10;
+function fun1() {
+    // Assign 5 to oopsGlobal Here
+    oopsGlobal = 5;
+}
+// Only change code above this line
+function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+        output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+}
+
+//Local Scope and Functions
+function myLocalScope() {
+    // Only change code below this line
+    let myVar;
+    console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
+
+//Global vs. Local Scope in Functions
+// Setup
+const outerWear = "T-Shirt";
+function myOutfit() {
+    // Only change code below this line
+    const outerWear  = "sweater";
+    // Only change code above this line
+    return outerWear;
+}
+myOutfit();
+
+//Understanding Undefined Value returned from a Function
+// Setup
+let sum = 0;
+function addThree() {
+    sum = sum + 3;
+}
+// Only change code below this line
+function addFive(){
+    sum +=5;
+}
+// Only change code above this line
+addThree();
+addFive();
+
+//Assignment with a Returned Value
+// Setup
+let processed = 0;
+function processArg(num) {
+    return (num + 3) / 5;
+}
+// Only change code below this line
+processed = processArg(7);
