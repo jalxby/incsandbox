02823fff047f1428441e6bba3140634e2e76f6ad
Aleksander Jaroshevich <jalxby@gmail.com>
2022-07-08 18:04:52 +0300

    Commit freecodecamp - 2

--- freecodecamp/BasicJavaScript/Store Multiple Values in one Variable using JavaScript Arrays.js
+++ freecodecamp/BasicJavaScript/Store Multiple Values in one Variable using JavaScript Arrays.js
@@ -1,2 +1,58 @@
 // Only change code below this line
 const myArray = ["string", 34];
+
+//Escape Sequences in Strings
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+//Concatenating Strings with Plus Operator
+
+const myStr = "This is the start. "+"This is the end."; // Change this line
+
+//Concatenating Strings with the Plus Equals Operator
+
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence."
+
+//Appending Variables to Strings
+
+// Change code below this line
+const someAdjective = "awesome";
+let myStr = "Learning to code is ";
+myStr += someAdjective
+
+//Find the Length of a String
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+//Use Bracket Notation to Find the First Character in a String
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+//Understand String Immutability
+
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+
+//Store Multiple Values in one Variable using JavaScript Arrays
+// Only change code below this line
+const myArray = ["string", 34];
+
+//Nest one Array within Another Array
+const myArray = [["JS", "React"],["Node","15"]];
+
+
