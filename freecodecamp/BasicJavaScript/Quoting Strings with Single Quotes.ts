97e00031f85c77224272c0ef1d1f346050a769f7
Aleksander Jaroshevich <jalxby@gmail.com>
2022-07-08 11:12:27 +0300

    Init commit

---
+++ freecodecamp/BasicJavaScript/Quoting Strings with Single Quotes.ts
@@ -0,0 +1,17 @@
+/*
+Change the provided string to a string with single quotes at the beginning and end and no escape characters.
+Right now, the <a> tag in the string uses double quotes everywhere. You will need to change the outer quotes to single
+quotes so you can remove the escape characters.
+
+ */
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+function boolToWord(bool) {
+    if (bool == true) {
+        return "Yes"
+    } else if (bool == false) {
+        return "No"
+    } else {
+        return "wrong parameter"
+    }
+}
