ee687751a6171f112e4566b641b7a49ae92149df
Aleksander Jaroshevich <jalxby@gmail.com>
2022-07-08 21:50:36 +0300

    Commit freecodecamp - 6

--- freecodecamp/BasicJavaScript/Store Multiple Values in one Variable using JavaScript Arrays.js
+++ freecodecamp/BasicJavaScript/Store Multiple Values in one Variable using JavaScript Arrays.js
@@ -400,3 +400,125 @@ function testSize(num) {
     // Only change code above this line
 }
 testSize(7);
+
+//Golf Code
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+    // Only change code below this line
+    if (strokes == 1){
+        return names[0]
+    }else if (strokes <= par - 2){
+        return names[1]
+    }else if(strokes == par - 1){
+        return names[2]
+    }else if(strokes == par){
+        return names[3]
+    }else if(strokes == par + 1){
+        return names[4]
+    }else if(strokes == par + 2){
+        return names[5]
+    }else if(strokes >= par + 3){
+        return names[6]
+    }else
+        return "Change Me";
+    // Only change code above this line
+}
+golfScore(5, 4);
+
+//Selecting from Many Options with Switch Statements
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch(val){
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "gamma";
+            break;
+        case 4:
+            answer = "delta";
+            break;
+    }
+    // Only change code above this line
+    return answer;
+}
+caseInSwitch(1);
+
+// Adding a Default Option in Switch Statements
+function switchOfStuff(val) {
+    let answer = "";
+    // Only change code below this line
+    switch(val){
+        case "a":
+            answer = "apple";
+            break;
+        case "b":
+            answer = "bird";
+            break;
+        case "c":
+            answer = "cat";
+            break;
+        default:
+            answer = "stuff";
+            break;
+    }
+    // Only change code above this line
+    return answer;
+}
+switchOfStuff(1);
+
+//Multiple Identical Options in Switch Statements
+function sequentialSizes(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High";
+            break;
+        // Only change code above this line
+    }return answer;
+}
+sequentialSizes(1);
+
+//Replacing If Else Chains with Switch
+function chainToSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch(val){
+        case "bob":
+            answer = "Marley";
+            break;
+        case 42:
+            answer = "The Answer";
+            break;
+        case 1:
+            answer = "There is no #1";
+            break;
+        case 99:
+            answer = "Missed me by this much!";
+            break;
+        case 7:
+            answer = "Ate Nine";
+            break;
+    }
+    // Only change code above this line
+    return answer;
+}
+chainToSwitch(7);
