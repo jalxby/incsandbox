14fefb4239010393165c5a9b0eb99260c1d2e4d2
Aleksander Jaroshevich <jalxby@gmail.com>
2022-07-08 18:15:00 +0300

    Commit freecodecamp - 3

--- freecodecamp/BasicJavaScript/Store Multiple Values in one Variable using JavaScript Arrays.js
+++ freecodecamp/BasicJavaScript/Store Multiple Values in one Variable using JavaScript Arrays.js
@@ -55,4 +55,40 @@ const myArray = ["string", 34];
 //Nest one Array within Another Array
 const myArray = [["JS", "React"],["Node","15"]];
 
+//Access Array Data with Indexes
+const myArray = [50, 60, 70];
+let myData = myArray[0];
+
+//Modify Array Data With Indexes
+// Setup
+const myArray = [18, 64, 99];
+// Only change code below this line
+myArray[0] = 45;
+
+//Access Multi-Dimensional Arrays With Indexes
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+];
+const myData = myArray[2][1];
+
+//Manipulate Arrays With push()
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+// Only change code below this line
+myArray.push(["dog", 3])
+
+//Manipulate Arrays With pop()
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+// Only change code below this line
+const removedFromMyArray = myArray.pop();
+
+//Manipulate Arrays With shift()
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+// Only change code below this line
+const removedFromMyArray = myArray.shift();
 
