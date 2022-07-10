ad6f84b51594768a49b8227eb2ea35adce8f2b6b
Aleksander Jaroshevich <jalxby@gmail.com>
2022-07-10 20:01:37 +0300

    Commit freecodecamp - 7

--- freecodecamp/BasicJavaScript/Store Multiple Values in one Variable using JavaScript Arrays.js
+++ freecodecamp/BasicJavaScript/Store Multiple Values in one Variable using JavaScript Arrays.js
@@ -522,3 +522,23 @@ function chainToSwitch(val) {
     return answer;
 }
 chainToSwitch(7);
+//Returning Boolean Values from Functions
+function isLess(a, b) {
+    // Only change code below this line
+    return ;
+    // Only change code above this line
+}
+isLess(10, 15);
+
+//Return Early Pattern for Functions
+// Setup
+function abTest(a, b) {
+    // Only change code below this line
+    if (a < 0 | b < 0){
+        return undefined;
+    }else{
+        // Only change code above this line
+        return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+    }
+}
+abTest(2,2);
